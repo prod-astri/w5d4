a1d8809efe0eec586a305b41ae8937178db3da8c
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-09-09 11:31:37 +0200

    some bug in my version

--- roles-authorization/models/Room.js
+++ roles-authorization/models/Room.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const roomSchema = new Schema({
+	// these values come from the user
 	name: String,
 	price: Number,
+	// this value will be put in by us
+	owner: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	}
 });
 
 const Room = mongoose.model('Room', roomSchema);
--- roles-authorization/routes/rooms.js
+++ roles-authorization/routes/rooms.js
@@ -1,18 +1,32 @@
 const router = require("express").Router();
 const Room = require('../models/Room');
-const { loginCheck } = require('./middlewares');
+// const { loginCheck } = require('./middlewares');
 
 
 router.get('/', (req, res, next) => {
-	// TODO
+	// show only the rooms that the logged in user created
+	Room.find({ owner: req.user._id })
+		.then(rooms => res.render('rooms/index', { roomList: rooms }))
+		.catch(err => next(err))
 });
 
 router.get('/add', (req, res, next) => {
 	res.render('rooms/add');
 });
 
-router.post('/', loginCheck(), (req, res, next) => {
-	// TODO
+router.post('/', (req, res, next) => {
+	const { name, price } = req.body;
+	// node-basic-auth req.session.user
+	console.log(req.user);
+	Room.create({
+		name: name,
+		price: price,
+		owner: req.user._id
+	})
+		.then(() => {
+			res.redirect('/rooms');
+		})
+		.catch(err => next(err));
 });
 
 router.get('/:id/delete', (req, res, next) => {
